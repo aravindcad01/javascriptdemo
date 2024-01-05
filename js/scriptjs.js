a2ea9d428d560cd54dcbb7b27451f6d48abb2f56
aravindcad01 <aravind2k23@gmail.com>
2024-01-05 17:54:59 +0530

    console added

--- js/scriptjs.js
+++ js/scriptjs.js
@@ -1,1 +1,41 @@
 console.log("ILAYATHALAPATHY");
+console.log("welcome to javascript");
+console.log('welcome to javascript');
+console.log(10.10);
+console.log(10.11);
+console.log(10.100);
+console.log(10.001);
+console.log({fname:"arav", gender:"male"});
+console.table({fname:"aravind",gender:"male"});
+console.log([1,10,100,1000]);
+console.table([1,10,100,1000]);
+console.log(true);
+var a=10;
+var b=20;
+const c=10;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+a=6;
+console.log(a);
+b=9;
+console.log(b);
+console.log(c);
+
+a=[12,11,25,60];
+console.log(a);
+console.table(a);
+
+a={fname:"aravind",gender:"male"};
+console.log(a);
+console.table(a);
+
+console.log("2"+"2"+"2"-2);
+
+a=1;
+console.log(typeof a);
+
+a=1.5;
+console.log(typeof a);
